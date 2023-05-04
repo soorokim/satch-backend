0c99ba7ec2c06a0cab2b221a917b395608bcf75a
soorokim <rlatnfh99@gmail.com>
2023-05-05 00:05:41 +0900

    fix: satchList를가져올때 date 내림차순으로 정렬하여 가져오도록함

--- src/goals/goals.service.ts
+++ src/goals/goals.service.ts
@@ -72,7 +72,7 @@ export class GoalsService {
       .doc(res.id)
       .collection('satchs');
 
-    const satchListnapshot = await satchsRef.get();
+    const satchListnapshot = await satchsRef.orderBy('date', 'desc').get();
     const satchList = [];
 
     satchListnapshot.forEach((doc) => {
